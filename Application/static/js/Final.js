d97f3699f9dcaac2a81b71cf1f17964ba93239cc
Eng-Hisham991 <engineer.hisham991@gmail.com>
2020-07-23 15:56:11 -0500

    Update

--- Application/static/js/Final.js
+++ Application/static/js/Final.js
@@ -135,3 +135,72 @@
   
 // // initial1();
 // // // optionChanged();
+airlines = [{'YV':'Mesa Airlines', 'EV':'ExpressJet','AS':'Alaska Airlines',
+'UA':'United Airlines','YX': 'Republic Airways','AA': 'American Airlines', 
+'F9':'Fronteir Airlines','9E':'Endeavor Air','NK':'Spirit Airlines','OO':'SkyWest Airlines',
+'DL':'Delta Airlines',"B6":'JetBlue',"OH":"PSA Airlines"}]; 
+
+var abbrev= [];
+var carrier = [];
+
+// Iterate through each recipe object
+airlines.forEach((airline) => {
+
+  // Iterate through each key and value
+  Object.entries(airline).forEach(([key, value]) => {
+    carrier.push(value)
+    abbrev.push(key)
+   });
+});
+
+
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+}  
+
+  
+  function initial1(){ 
+
+    d3.csv("/data").then((data)=>{
+        console.log(data)
+        var airlines = data.map(item=>item.Airline).filter( onlyUnique ); 
+        var dropDown1 = d3.select("#selDataset");
+        console.log(airlines)
+
+        carrier.forEach((sample) => {
+        
+        dropDown1
+        .append("option")
+        .text(sample)
+        .property("value", sample);  
+
+        });
+        console.log(dropDown1.length)
+        var dest_airports = data.map(item=>item.DEST_AIRPORT).filter( onlyUnique ); 
+        var dropDown2 = d3.select("#selDataset1");
+
+        dest_airports.forEach((sample) => {
+            dropDown2
+            .append("option")
+            .text(sample)
+            .property("value", sample);   
+
+
+        });
+
+        
+        
+    });
+   
+         
+  };
+  
+  
+  function optionChanged(newSample) {
+
+  };
+  
+
+  
+initial1();
+optionChanged();
